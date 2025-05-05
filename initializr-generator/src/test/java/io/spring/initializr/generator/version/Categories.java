83618d745c676de1528e8e824d4ec11e89c0963b
bhavya1099 <kinshu.1019@gmail.com>
2025-05-05 06:47:50 +0000

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ initializr-generator/src/test/java/io/spring/initializr/generator/version/Categories.java
@@ -0,0 +1,33 @@
+package io.spring.initializr.generator.version;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface ofProperty {
+
+	}
+
+	interface ofValue {
+
+	}
+
+	interface isProperty {
+
+	}
+
+}
